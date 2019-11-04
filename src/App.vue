9f0a967c6b41dd0ab0b6e3bb08098739c7f3a033
Brian Nelson <ladofmusi@outlook.com>
2019-11-04 13:07:54 -0700

    added toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
 <template>
 <div id="app">
     <div id="nav">
+<v-toolbar dense>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+  
+        <v-toolbar-title>Title</v-toolbar-title>
+  
+        <v-spacer></v-spacer>
+  
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+      </v-toolbar>
+
+
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link to="/form">Form</router-link>
@@ -11,19 +24,19 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+//import HelloWorld from './components/HelloWorld';
 
 
 export default {
   name: "App",
 
   components: {
-    HelloWorld,
+    //HelloWorld,
     
   },
 
   data: () => ({
-    //
+   
   })
 };
 </script>
